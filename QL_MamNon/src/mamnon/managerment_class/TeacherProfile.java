cd8805e05d5434e4a9f07ea5ccc9be0d7f70d0b7
dungvan2512 <dungvan2512@gmail.com>
2017-04-21 21:21:07 +0700

    test

---
+++ QL_MamNon/src/mamnon/managerment_class/TeacherProfile.java
@@ -0,0 +1,5 @@
+package mamnon.managerment_class;
+
+public class TeacherProfile extends UserProfile{
+
+}
---
+++ QL_MamNon/src/mamnon/model/Authentication.java
@@ -0,0 +1,108 @@
+package mamnon.model;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import mamnon.database.DatabaseConnection;
+
+/**
+ * Servlet implementation class Authentication
+ */
+
+public class Authentication extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public Authentication() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+
+		String id = (String) request.getParameter("ID");
+		String password = (String) request.getParameter("password");
+
+		if (checkAuthentication(id, password)) {
+
+			if (id.startsWith("HS")) {
+				
+				HttpSession session = request.getSession();
+				session.setAttribute("ID", id);
+				session.setMaxInactiveInterval(30);
+				RequestDispatcher dispatcher = request.getRequestDispatcher("view/index.jsp");
+				dispatcher.forward(request, response);
+
+			} else if (id.startsWith("GV")) {
+
+				HttpSession session = request.getSession();
+				session.setAttribute("ID", id);
+				session.setMaxInactiveInterval(30);
+				RequestDispatcher dispatcher = request.getRequestDispatcher("student");
+				dispatcher.forward(request, response);
+			}
+		} else {
+
+			response.sendRedirect("view/index.jsp");
+
+		}
+
+	}
+
+	public static boolean checkAuthentication(String username, String password) {
+
+		String sql = "select Password from taikhoan where ID = '" + username + "'";
+
+		try {
+			Connection conn = DatabaseConnection.getConnection();
+			Statement stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery(sql);
+			if (rs.next() && rs.getObject("Password").toString().trim().equals(password)) {
+				rs.close();
+				stmt.close();
+				conn.close();
+				return true;
+			} else {
+				rs.close();
+				stmt.close();
+				conn.close();
+				return false;
+			}
+		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return false;
+	}
+
+}
